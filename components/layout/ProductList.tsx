7288242fba1d5464613a9f5e1fa8f4a15b33e6ae
misbahul45 <misbahulmuttaqin395@example.com>
2024-10-08 22:25:33 +0700

    push

--- components/layout/ProductList.tsx
+++ components/layout/ProductList.tsx
@@ -17,10 +17,10 @@ const ProductList = ({ categoryId, categories,images,name, price, slug, id, togg
     const categoryProducts=categories.find((category)=>category.id===categoryId)
   return (
     <Link onClick={()=>toggleSearch()} href={'/products/'+slug} key={id+''} className="flex gap-2 hover:bg-slate-200 cursor-pointer p-2 rounded-lg">
-        <Image src={images[0]} width={120} height={120} alt={name} className="size-32 rounded-lg shadow-xl shadow-slate-200 object-cover" />
+        <Image src={images[0]} width={120} height={120} alt={name} className="size-32 rounded-lg object-cover shadow-xl shadow-slate-700/40" />
         <div>
             <p>{name}</p>
-            <button className='font-bold lg:my-4 my-2 text-sm bg-slate-200 shadow-xl shadow-black/20 px-3 py-1 rounded '>${price}</button>
+            <button className='font-bold lg:my-4 my-2 text-sm bg-slate-200 shadow-xl shadow-black/20 px-3 py-1 rounded '>{price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</button>
             <p>{categoryProducts?.category}</p>
         </div>
     </Link>
