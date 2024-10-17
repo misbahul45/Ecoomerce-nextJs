b6424bd5fee91b95cc8a026af0e58a4d3caf5b93
misbahul45 <misbahulmuttaqin395@example.com>
2024-10-17 13:06:36 +0700

    completed my cool projects

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,61 +1,54 @@
-import { getAllLength, getAllValuesByMonth } from '@/actions/dashboard.action'
-import Chart from '@/components/dashboard/Chart'
-import { Metadata } from 'next'
-import React from 'react'
-import { FaMoneyCheckAlt, FaRegImages, FaRegWindowRestore } from 'react-icons/fa'
-import { HiUsers } from 'react-icons/hi'
-import { LiaCommentSolid } from 'react-icons/lia'
-import { MdCategory } from 'react-icons/md'
+"use client";
 
-const ICON={
-  "User":<HiUsers size={18} />,
-  "Product":<FaRegWindowRestore size={18} />,
-  "Category":<MdCategory size={18} />,
-  "Coment":<LiaCommentSolid size={18} />,
-  "Order":<FaMoneyCheckAlt size={18} />,
-  "Poster":<FaRegImages size={18} />
-}
+import { getAllLength, getAllValuesByMonth } from '@/actions/dashboard.action';
+import Chart from '@/components/dashboard/Chart';
+import { Metadata } from 'next';
+import React, { useEffect, useState } from 'react';
+import { FaMoneyCheckAlt, FaRegImages, FaRegWindowRestore } from 'react-icons/fa';
+import { HiUsers } from 'react-icons/hi';
+import { LiaCommentSolid } from 'react-icons/lia';
+import { MdCategory } from 'react-icons/md';
 
-export const metadata:Metadata = {
-  title: "Dashboard - Misbahul's Shop",
-  description: "Dashboard for Misbahul's Shop",
-}
+const ICON = {
+  "User": <HiUsers size={18} />,
+  "Product": <FaRegWindowRestore size={18} />,
+  "Category": <MdCategory size={18} />,
+  "Coment": <LiaCommentSolid size={18} />,
+  "Order": <FaMoneyCheckAlt size={18} />,
+  "Poster": <FaRegImages size={18} />
+};
 
-const page = async() => {
-  const {usersLength,productsLength,categoriesLength,comentsLength,ordersLength,postersLength} = await getAllLength();
-  const allData=[
-    {
-      name:'User',
-      count:usersLength
-    },
-    {
-      name:'Product',
-      count:productsLength
-    },
-    {
-      name:'Category',
-      count:categoriesLength
-    },
-    {
-      name:'Coment',
-      count:comentsLength
-    },
-    {
-      name:'Order',
-      count:ordersLength
-    },
-    {
-      name:'Poster',
-      count:postersLength
-    }
-  ]
 
-  const chartData=await getAllValuesByMonth();
+const Page = () => {
+  const [allData, setAllData] = useState<any[]>([]);
+  const [chartData, setChartData] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { usersLength, productsLength, categoriesLength, comentsLength, ordersLength, postersLength } = await getAllLength();
+      
+      const data = [
+        { name: 'User', count: usersLength },
+        { name: 'Product', count: productsLength },
+        { name: 'Category', count: categoriesLength },
+        { name: 'Coment', count: comentsLength },
+        { name: 'Order', count: ordersLength },
+        { name: 'Poster', count: postersLength }
+      ];
+
+      setAllData(data);
+
+      const chart = await getAllValuesByMonth();
+      setChartData(chart);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='w-full h-full py-2'>
       <div className="w-full lg:max-w-5xl max-w-[80%] mx-auto flex gap-6 items-center justify-center flex-wrap px-6">
-        {allData?.map((item,index)=>(
+        {allData?.map((item, index) => (
           <div key={index} className='flex flex-col justify-center h-full items-center flex-1 py-2 px-4 shadow-xl shadow-slate-700/20 rounded-lg animate-1-pulse'>
             <p className='lg:text-3xl md:Text-2xl text-sm font-bold'>{item.count}</p>
             <div className="flex gap-3 items-center">
@@ -67,7 +60,7 @@ const page = async() => {
       </div>
       <Chart chartData={chartData} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
